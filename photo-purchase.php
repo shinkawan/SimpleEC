170ff733557148144ff798ce353777b18d1dee87
Artflair <dev@artflair.co.jp>
2026-04-01 13:44:58 +0900

    Release: Version 4.2.4 - Stabilize reorder feature, fix variation data preservation and standard product matching, and update documentation.

--- photo-purchase.php
+++ photo-purchase.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Simple EC
  * Description: 簡易的な写真・デジタルコンテンツ販売プラグイン。Stripe、PayPay、代引き、銀行振込に対応。
- * Version: 4.0.0
+ * Version: 4.2.4
  * Author: アートフレア株式会社
  * Author URI: https://www.artflair.co.jp/
  * Text Domain: photo-purchase
@@ -13,7 +13,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define constants
-define('PHOTO_PURCHASE_VERSION', '4.0.0');
+define('PHOTO_PURCHASE_VERSION', '4.2.4');
 define('PHOTO_PURCHASE_PATH', plugin_dir_path(__FILE__));
 define('PHOTO_PURCHASE_URL', plugin_dir_url(__FILE__));
 
